b976af9a282bcd80ac60b329164a5afff526721c
MarekSroka <marsrok@gmail.com>
2024-05-23 08:11:48 +0200

    Version 2.1 updates

--- read_attributes_version_2.py
+++ read_attributes_version_2.py
@@ -1,10 +1,11 @@
-  
 # LIBRARIES
+
 import os
 import xml.etree.ElementTree as ET
 import pandas as pd
 
 # CODE
+
 def process_attribute(attribute_element):
     attr_dict = {}
     for child in attribute_element:
@@ -22,6 +23,8 @@ def process_xml_file(file_path):
     object_type_element = root.find('.//businessObject/objectType')
     object_type = object_type_element.text.strip() if object_type_element is not None and object_type_element.text and object_type_element.text.strip() else '<none>'
 
+    file_name = os.path.basename(file_path)
+    
     attribute_list = root.find('.//businessObject/attributeList')
     
     attributes = []
@@ -30,10 +33,12 @@ def process_xml_file(file_path):
         attributes.append(processed_attribute)
         
     df = pd.DataFrame(attributes)
-    
-    file_name = os.path.basename(file_path)
     df.insert(0, 'object_type', object_type)
     df.insert(1, 'file_name', file_name)
+    
+    if 'valueType' in df.columns:
+        columns = [col for col in df.columns if col != 'valueType'] + ['valueType']
+        df = df[columns]
     
     return df
 
@@ -56,13 +61,12 @@ folder_path = input('Enter the main folder to process: ')
 
 dfs, file_names = process_folder(folder_path)
 
-output_file = 'Attributes.xlsx'
+output_file = 'XML_Attributes.xlsx'
 with pd.ExcelWriter(output_file) as writer:
-    for object_type, df_list in dfs.items():
-        combined_df = pd.concat(df_list, ignore_index=True)
+    for object_type in sorted(dfs.keys()):
+        combined_df = pd.concat(dfs[object_type], ignore_index=True)
         combined_df.to_excel(writer, sheet_name=object_type, index=False)
 
 print(f'Totally added: {len(file_names)} files to report {output_file}:')
 for file_name in file_names:
     print(f'- {file_name}')
-
